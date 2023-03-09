d8cdb32321c4aa8ba843d3579c344acde502723e
anishDalvi <anishdalvi2001@gmail.com>
2023-03-10 04:08:08 +0530

    init

---
+++ expense_tracker/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/signin.dart';
+
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Signin(),
+  ));
+}
